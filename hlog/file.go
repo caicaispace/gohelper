887135e85f3eadc7d5c83613d101e082bd5c77c6
Cai.Cai <yangcai.email@gmail.com>
2022-06-29 23:39:31 +0800

    update

---
+++ hlog/file.go
@@ -0,0 +1,22 @@
+package hlog
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/caicaispace/gohelper/setting"
+)
+
+// getLogFilePath get the log file save path
+func getLogFilePath() string {
+	return fmt.Sprintf("%s%s", setting.App.LogPath, "")
+}
+
+// getLogFileName get the save name of the log file
+func getLogFileName() string {
+	return fmt.Sprintf("%s%s.%s",
+		setting.App.LogPrefix,
+		time.Now().Format(setting.App.TimeFormat),
+		setting.App.LogExtension,
+	)
+}
